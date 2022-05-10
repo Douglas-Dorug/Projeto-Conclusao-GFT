43bbf51a8d79d3c789a4656f30ca7cca3e9e2467
Douglas-Dorug <douglasx30@gmail.com>
2022-05-10 00:18:08 -0300

    Desenvolvidos services para funcionamento do codigo e adicionado readme.md

---
+++ README.md
@@ -0,0 +1,36 @@
+**SOBRE O QUE É O PROJETO?**
+========================================================================
+Projeto desenvolvido para conclusão do bootcamp GFT Start #5 Java.
+
+O projeto consiste em uma API que simula uma aplicação de gerenciamento e possibilita ao usuário, realizar o CRUD de clientes.
+
+**Tecnologias:**
+- Java ![Badge](https://img.shields.io/badge/Java-v11-red)
+- Git
+- Lombok  ![Badge](https://img.shields.io/badge/Lombok-vv1.18.20-red)
+- Maven ![Badge](https://img.shields.io/badge/Maven-v3.8.4-red)
+- Spring Boot ![Badge](https://img.shields.io/badge/Spring-v2.6.7-green)
+- Api (Rest)
+- Swagger ![Badge](https://img.shields.io/badge/Swagger-v2.9.2-green)
+- JPA (Hibernate)
+- Spring Data
+- H2 ![Badge](https://img.shields.io/badge/H2Db-important)
+
+**Iniciando o Back-End**
+========================================================================
+Para a criação e execução do progeto foi utilizada o Intellij como IDE para desenvolvimento java onde foram instaladas
+as dependencias necessárias para o funcionamento.
+
+**Back-End**
+
+	Primeiramente deve-se clonar o repositório no endereço:
+	
+	https://github.com/Douglas-Dorug/Projeto-Conclusao-GFT
+	
+	Após o projeto ser clonado, execute a aplicação que se encontra em 
+    ..src/main/java/one/digitalinnovation/projetopadroesjavaspring/ProjetoPadroesJavaSpringApplication.java
+    Feito isto, o back-end estará funcionando
+
+    Obs: Não é necessário subir nenhum banco de dados, pois o projeto utiliza o banco de dados em memoria H2.
+
+Para acessar a API, utilize o endereço: http://localhost:8080/swagger-ui.html#/ no browser.
--- src/main/java/one/digitalinnovation/projetopadroesjavaspring/service/implementacao/ClienteServiceImplement.java
+++ src/main/java/one/digitalinnovation/projetopadroesjavaspring/service/implementacao/ClienteServiceImplement.java
@@ -1,33 +1,66 @@
 package one.digitalinnovation.projetopadroesjavaspring.service.implementacao;
 
 import one.digitalinnovation.projetopadroesjavaspring.model.entity.Cliente;
+import one.digitalinnovation.projetopadroesjavaspring.model.entity.Endereco;
+import one.digitalinnovation.projetopadroesjavaspring.model.repository.ClienteRespository;
+import one.digitalinnovation.projetopadroesjavaspring.model.repository.EnderecoRepository;
 import one.digitalinnovation.projetopadroesjavaspring.service.ClienteService;
+import one.digitalinnovation.projetopadroesjavaspring.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImplement implements ClienteService {
+
+    @Autowired
+    private ClienteRespository clienteRespository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRespository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRespository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        salvarClienteComCep(cliente);
     }
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
-
+        Optional<Cliente> clienteBancoDeDados = clienteRespository.findById(id);
+        if (clienteBancoDeDados.isPresent()){
+            salvarClienteComCep(cliente);
+        }
     }
 
     @Override
     public void deletar(Long id) {
+        clienteRespository.deleteById(id);
+    }
 
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById((cep))
+                .orElseGet(() -> {
+                    Endereco novoEndereco = viaCepService.consultarCep(cep);
+                    enderecoRepository.save(novoEndereco);
+                    return novoEndereco;
+                });
+        cliente.setEndereco(endereco);
+        clienteRespository.save(cliente);
     }
 }
